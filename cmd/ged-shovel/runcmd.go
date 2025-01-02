51929b5b463ad8011e493367876211e2e108683b
Peter Magnusson <me@kmpm.se>
2025-01-02 11:44:17 +0100

    fix: adjust time buckets

--- cmd/ged-shovel/runcmd.go
+++ cmd/ged-shovel/runcmd.go
@@ -25,7 +25,7 @@ var (
 	messageDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    "ged_shovel_message_duration",
 		Help:    "The duration of messages",
-		Buckets: []float64{0.000001, 0.000005, 0.00001, 0.00005, .0001, .0005, .001, .005, .01, .05, .1},
+		Buckets: []float64{.0000001, .000001, .00001, .0001, .001, .01, .1, 1},
 	}, []string{"status"})
 )
 
